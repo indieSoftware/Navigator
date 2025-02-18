4faf732ed825608d4e5d52a9c249a50d449986ec
Michael Long <hmlong@gmail.com>
2025-02-18 15:56:51 -0600

    Update online docs

--- NavigatorDemo/NavigatorDemo/NavigatorDemo/Services/Logging.swift
+++ NavigatorDemo/NavigatorDemo/NavigatorDemo/Services/Logging.swift
@@ -16,3 +16,42 @@ public struct Logger: Logging {
         print(message)
     }
 }
+
+public struct FeedItem: Codable {
+    let title: String
+}
+
+enum MyError: Error {
+    case connectivity
+    case invalidData
+    case badStatus(Int)
+}
+
+public func load(url: URL) async throws -> [FeedItem] {
+    do {
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw MyError.invalidData
+        }
+        guard httpResponse.statusCode == 200 else {
+            throw MyError.badStatus(httpResponse.statusCode)
+        }
+        return try JSONDecoder().decode([FeedItem].self, from: data)
+    } catch is DecodingError {
+        throw MyError.invalidData
+    } catch {
+        throw MyError.connectivity
+    }
+}
+
+extension Result {
+    func onSuccess(_ handler: (Success) -> Void, failure: (Failure) -> Void) -> Self {
+        switch self {
+        case .success(let value):
+            handler(value)
+        case .failure(let error):
+            failure(error)
+        }
+        return self
+    }
+}
