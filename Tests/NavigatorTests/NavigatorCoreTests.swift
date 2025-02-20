5fc17980bbf6f24543dd85ab67dadc2a65e8ef7e
Michael Long <hmlong@gmail.com>
2025-02-20 15:43:17 -0600

    Navigator basic tests

--- Tests/NavigatorTests/NavigatorCoreTests.swift
+++ Tests/NavigatorTests/NavigatorCoreTests.swift
@@ -1,5 +1,5 @@
 //
-//  NavigatorTests.swift
+//  NavigatorCoreTests.swift
 //  Navigator
 //
 //  Created by zzmasoud on 02/15/25.
@@ -9,7 +9,8 @@ import Testing
 import Foundation
 @testable import Navigator
 
-struct NavigatorTests {
+struct NavigatorCoreTests {
+
     // MARK: - Basic Navigation State Tests
 
     @Test func testNavigatorInitialization() {
@@ -33,53 +34,6 @@ struct NavigatorTests {
         #expect(childNavigator.parent?.id == parentNavigator.id)
         #expect(childNavigator.root.id == parentNavigator.id)
         #expect(childNavigator.state.isPresented)
-    }
-
-    // MARK: - Navigation Path Management
-
-    @Test func testNavigationPathOperations() async {
-        let navigator = Navigator(owner: .root)
-        let destination = MockDestination.screen1
-        
-        // Test push
-        await navigator.push(destination)
-        #expect(navigator.state.path.count == 1)
-        
-        // Test pop
-        let popped = await navigator.pop()
-        #expect(popped)
-        #expect(navigator.state.path.isEmpty)
-    }
-
-    @Test func testMultiplePathOperations() async {
-        let navigator = Navigator(owner: .root)
-        
-        // Push multiple destinations
-        await navigator.push(MockDestination.screen1)
-        await navigator.push(MockDestination.screen2)
-        await navigator.push(MockDestination.screen3)
-        #expect(navigator.state.path.count == 3)
-
-        // Pop to root
-        let popped = await navigator.popAll()
-        #expect(popped)
-        #expect(navigator.state.path.isEmpty)
-    }
-
-    // MARK: - Sheet Presentation Tests
-
-    @Test func testSheetPresentation() async {
-        let navigator = Navigator(owner: .root)
-        let destination = MockDestination.screen1
-
-        // Present sheet
-        await navigator.navigate(to: destination, method: .sheet)
-        #expect(navigator.state.sheet != nil)
-        #expect(navigator.state.sheet?.wrapped as? MockDestination == destination)
-
-        // Dismiss sheet
-        await navigator.dismissPresentedViews()
-        #expect(navigator.state.sheet == nil)
     }
 
     // MARK: - Navigation Lock Tests
@@ -121,4 +75,5 @@ struct NavigatorTests {
         parent.state.removeChild(child1)
         #expect(parent.state.children.count == 1)
     }
+    
 }
