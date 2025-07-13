9a5149fda1334b373c5a56d2378fe02067957410
Michael Long <hmlong@gmail.com>
2025-07-13 15:07:58 -0500

    Fix navigate(to:) modifier issue

--- Sources/NavigatorUI/NavigatorUI/Core/NavigationOperations.swift
+++ Sources/NavigatorUI/NavigatorUI/Core/NavigationOperations.swift
@@ -181,7 +181,7 @@ extension Navigator {
 extension View {
 
     public func navigate(to destination: Binding<(some NavigationDestination)?>) -> some View {
-        self.modifier(NavigateToModifier(destination: destination, method: destination.wrappedValue?.method ?? .push))
+        self.modifier(NavigateToModifier(destination: destination, method: nil))
     }
 
     public func navigate(to destination: Binding<(some NavigationDestination)?>, method: NavigationMethod) -> some View {
@@ -189,7 +189,7 @@ extension View {
     }
 
     public func navigate(trigger: Binding<Bool>, destination: some NavigationDestination) -> some View {
-        self.modifier(NavigateTriggerModifier(trigger: trigger, destination: destination, method: destination.method))
+        self.modifier(NavigateTriggerModifier(trigger: trigger, destination: destination, method: nil))
     }
 
     public func navigate(trigger: Binding<Bool>, destination: some NavigationDestination, method: NavigationMethod) -> some View {
@@ -246,13 +246,13 @@ extension NavigationState {
 
 private struct NavigateToModifier<T: NavigationDestination>: ViewModifier {
     @Binding internal var destination: T?
-    internal let method: NavigationMethod
+    internal let method: NavigationMethod?
     @Environment(\.navigator) internal var navigator: Navigator
     func body(content: Content) -> some View {
         content
             .onChange(of: destination) { destination in
                 if let destination {
-                    navigator.navigate(to: destination, method: method)
+                    navigator.navigate(to: destination, method: method ?? destination.method)
                     self.destination = nil
                 }
             }
@@ -262,13 +262,13 @@ private struct NavigateToModifier<T: NavigationDestination>: ViewModifier {
 private struct NavigateTriggerModifier<T: NavigationDestination>: ViewModifier {
     @Binding internal var trigger: Bool
     let destination: T
-    internal let method: NavigationMethod
+    internal let method: NavigationMethod?
     @Environment(\.navigator) internal var navigator: Navigator
     func body(content: Content) -> some View {
         content
             .onChange(of: trigger) { trigger in
                 if trigger {
-                    navigator.navigate(to: destination, method: method)
+                    navigator.navigate(to: destination, method: method ?? destination.method)
                     self.trigger = false
                 }
             }
